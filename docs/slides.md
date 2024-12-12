7d68dc4fe8018f11b6d5d8a41a00ed10755e8175
Matteo Ferrini <maf@lunar.app>
2024-12-12 16:05:45 +0100

    Add presentation

---
+++ docs/slides.md
@@ -0,0 +1,50 @@
+---
+marp: true
+---
+
+# TUIs in Python
+
+---
+
+# CLIs and GUIs
+
+![width:600px](assets/cli_vs_gui.png)
+
+---
+
+
+# TUIs: the middleground
+
+![width:500px](assets/tui.jpg)
+
+---
+
+# Textualize
+
+Textual is a framework to build TUIs in Python.
+
+Features:
+- easy
+- fast
+- efficient
+- open source
+- cross platform
+- pretty
+
+---
+
+# Examples - *tui-network*
+
+![width:900px](https://github.com/Zatfer17/tui-network/raw/main/docs/gruvbox.png)
+
+---
+
+# Examples - *harlequin*
+
+![width:900px](https://github.com/tconbeer/harlequin/raw/main/harlequin.svg)
+
+---
+
+# Examples - *upiano*
+
+![width:800px](https://github.com/eliasdorneles/upiano/raw/master/screenshot-upiano.png)
--- tui_notes/main.py
+++ tui_notes/main.py
@@ -1,47 +1,10 @@
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, HorizontalScroll, VerticalScroll
-from textual.widgets import Footer, DirectoryTree, TextArea, Markdown, MarkdownViewer, Input
-from ollama import chat
+from textual.containers import HorizontalScroll
+from textual.widgets import Footer, TextArea
+from widgets.files import FilesWidget
+from widgets.note import NoteWidget
+from widgets.assistant import AssistantWidget
 
-
-class FilesWidget(VerticalScroll):
-
-    BORDER_TITLE = 'Files'
-
-    def compose(self) -> ComposeResult:
-        yield DirectoryTree('./notes/')
-
-    def on_directory_tree_file_selected(self, message: DirectoryTree.FileSelected):
-        if str(message.path).endswith('.md'):
-            with open(message.path) as f:
-                self.app.query_one(NoteWidget).note_path = str(message.path)
-                self.app.query_one(TextArea).load_text(f.read())
-
-class NoteWidget(HorizontalScroll):
-
-    BORDER_TITLE = 'Note'
-
-    def compose(self) -> ComposeResult:
-        yield VerticalScroll(TextArea())
-        yield VerticalScroll(MarkdownViewer(show_table_of_contents=False))
-    
-    def on_text_area_changed(self, message: TextArea.Changed) -> None:
-        self.query_one(Markdown).update(message.text_area.text)
-
-class AssistantWidget(VerticalScroll):
-
-    BORDER_TITLE = 'Assistant'
-
-    def compose(self) -> ComposeResult:
-        yield Input(id='assistant')
-
-    def on_input_submitted(self, message: Input.Submitted) -> None:
-        response = chat(
-            model='llama3.2:latest',
-            messages=[{'role': 'user', 'content': f'Given this context: {self.app.query_one(TextArea).text}, please: {message.value}'}]
-        )
-        self.app.query_one(TextArea).clear()
-        self.app.query_one(TextArea).load_text(response['message']['content'])
 
 class NotesApp(App):
 
@@ -56,7 +19,7 @@ class NotesApp(App):
         with HorizontalScroll(id='main_view'):
             yield FilesWidget(can_focus=False)
             yield NoteWidget(can_focus=False)
-        yield AssistantWidget()
+        #yield AssistantWidget()
         yield Footer()
 
     def action_save_note(self) -> None:
